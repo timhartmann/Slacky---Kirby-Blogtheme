02d93d04af17cff5aefd4e1a913f9c5fbad2f8c4
Tim Hartmann <tim.hartmann93@gmail.com>
2012-05-13 14:23:11 +0200

    bugfix

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -1,7 +1,7 @@
   <footer class="footer">
   	<div class="container_12 group">
   		<span class="small-description"><?php echo kirbytext($site->description()) ?></span>
-    	<span class="copyright"<?php echo kirbytext($site->copyright()) ?></span>
+    	<span class="copyright"><?php echo kirbytext($site->copyright()) ?></span>
     </div>
   </footer>
 
